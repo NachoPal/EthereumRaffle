6dbae6e37906c820d60f46fe35fbb4d049b19128
Nacho <ignacio.palacios.santos@gmail.com>
2017-12-30 17:45:49 -0600

    Adding a user done

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -2,9 +2,48 @@ pragma solidity ^0.4.19;
 
 
 contract Raffle {
-    function Raffle(){
 
+    //ESTOY AQUI AÑADIENDO EVENTOS
+    event PlayerAdded(string name, uint numAttempts, int balance);
+
+
+    struct Player {
+        uint index;
+        bool exists;
+        string name;
+        uint numAttempts;
+        int balance;
     }
 
+    address[] playersAddresses;
+
+    mapping (address => Player) public players;
+
+    function isAregisteredPlayer(address _address) return(bool registered){
+        return players[_address].exists
+    }
+
+    function registerPlayer(string _name) {
+        //Check if the Player already exist
+        require(isARegisteredPlayer(msg.sender) == false);
+
+        //Save in addresses array
+        uint index = playersAddresses.push(msg.sender) - 1;
+
+        players[msg.sender] = Player(index,true,_name,0,0);
+    }
+
+    function getPlayersCount() public constant returns(uint count) {
+        return playersAddressIndex.length;
+    }
+
+    function getPlayerAddressAtIndex(uint _index) public view return(address playerAddress){
+        return playersAddresses[index];
+    }
+
+    function getPlayerByAddress(address _playerAddress) public view return(string name, uint numAttemps, int balance) {
+        require(isAregisteredPlayer(address _playerAddress));
+        players[_playerAddress];
+    }
 
 }
